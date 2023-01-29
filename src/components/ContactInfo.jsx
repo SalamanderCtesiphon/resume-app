51efb4318437da63d20769e890ec90ece4f426a7
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-29 10:11:34 -0600

    working throught the project but i am having a hard time deciding how to do it

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,5 +1,43 @@
-const ContactInfo = () => {
-    return <h1>contactInfo</h1>
+import React, { useState } from "react"
+import '../App.css'
+
+const ContactInfo = (intialValues = {}) => {
+    const [inputs, setInputs] = useState(intialValues);
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        
+        console.log(inputs);
+        setInputs('');
+    };
+
+    function handleChange(e) {
+        setInputs({[e.target.name]: e.target.value})
+    };
+
+    return (
+        <div className="contactForm">
+            <h3>Contact Information</h3>
+            <form onSubmit={handleSubmit} className="contactInputs">
+                <label>First Name:</label>
+                <input 
+                    type="text"
+                    name="firstName" 
+                    value={inputs.firstName || ''}
+                    onChange={handleChange}
+                />
+                <label>Last Name:</label>
+                <input 
+                    type="text"
+                    name="lastName" 
+                    value={inputs.lastName || ''}
+                    onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default ContactInfo
