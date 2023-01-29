883c94aa10050fa1ccf8921c638fbdc979c2f792
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-29 16:37:43 -0600

    the main functionality of the contact section up and running have it set to toggle the input form from shown to just a button on submit.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import ContactInfo from './components/ContactInfo';
 function App(props) {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [streetAddress, setStreetAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [zip, setZip] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [contactSubmit, setContactSubmit] = useState(false);
+  
 
 
   function handleSubmit(e) {
@@ -12,7 +20,19 @@ function App(props) {
       
       setFirstName(firstName);
       setLastName(lastName);
+      setStreetAddress(streetAddress);
+      setCity(city);
+      setState(state);
+      setZip(zip);
+      setEmail(email);
+      setPhone(phone);
+      setContactSubmit(false);
   };
+
+  function toggleContact(e) {
+    console.log('click')
+    setContactSubmit(true);
+  }
 
   return (
     <div className="App">
@@ -21,14 +41,37 @@ function App(props) {
         <ContactInfo
           firstName={firstName}
           lastName={lastName}
+          streetAddress={streetAddress}
           handleSubmit={handleSubmit}
           setFirstName={setFirstName}
           setLastName={setLastName}
+          setStreetAdress={setStreetAddress}
+          city={city}
+          setCity={setCity}
+          state={state}
+          zip={zip}
+          email={email}
+          setState={setState}
+          setZip={setZip}
+          setEmail={setEmail}
+          phone={phone}
+          setPhone={setPhone}
+          contactSubmit={contactSubmit}
+          toggleContact={toggleContact}
         />
       </div>
       <div className="page">
-        {firstName}
-        {lastName}
+        <div className="pageHeader">
+          <div className="headerLeft">
+            <h3>{firstName}{' '}{lastName}</h3>
+            <h5>{streetAddress}</h5>
+            <h5>{city}{' '}{state}{' '}{zip}</h5>
+          </div>
+          <div className="headerRight">
+            <h5>{email}</h5>
+            <h5>{phone}</h5>
+          </div>
+        </div>
       </div>
     </div>
   );
